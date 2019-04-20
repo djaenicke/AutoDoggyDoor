1743597ee871d91cea409eee26fe30cecf137667
Lacy <laccomer@iu.edu>
2019-04-20 12:23:42 -0400

    Create ultrasonic.h
    Changed code completely

---
+++ ece595_project_2/source/ultrasonic.h
@@ -0,0 +1,14 @@
+/*
+ * ultrasonic.h
+ *
+ *  Created on: Apr 19, 2019
+ *      Author: Lacy
+ */
+
+#ifndef ULTRASONIC_H_
+#define ULTRASONIC_H_
+
+extern void Init_USS_Timer(void);
+extern void Detect_Pet(void);
+
+#endif /* ULTRASONIC_H_ */
